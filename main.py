3fa2ecb15b0098f0c09c2e0fbb624c7e371def36
Clark He <>
2020-01-03 16:52:10 +0800

    test commit

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ from trainer import BaseTrainer
 os.environ['CUDA_VISIBLE_DEVICES'] = '0, 1'
 
 
+# Test commit
+
 def main():
     output_dir = cfg.OUTPUT_DIR
     if output_dir and not os.path.exists(output_dir):
